8ad89edd0c3092caf36b3690bf45c5d3381ce59c
vijay <shrivijay24@gmail.com>
2021-01-24 15:54:58 +0530

    Updates

---
+++ precache-manifest.944b1fae429d8352d7b79b4cd8d8f222.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84816a9cd89a23b7d5874bd79f8fd673",
+    "url": "/vegcart2/index.html"
+  },
+  {
+    "revision": "75ca2e1a5780072de50f",
+    "url": "/vegcart2/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "d9e1c54406f857775ed9",
+    "url": "/vegcart2/static/css/main.2c5812dc.chunk.css"
+  },
+  {
+    "revision": "75ca2e1a5780072de50f",
+    "url": "/vegcart2/static/js/2.fa1baf00.chunk.js"
+  },
+  {
+    "revision": "9f36583424cf900ca3e6c23446e83813",
+    "url": "/vegcart2/static/js/2.fa1baf00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e1c54406f857775ed9",
+    "url": "/vegcart2/static/js/main.bdeda30e.chunk.js"
+  },
+  {
+    "revision": "48dc7e934be3eed40f0c",
+    "url": "/vegcart2/static/js/runtime-main.71a70bd1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.dde2ee0c2041ffa1ed0a78c021bfb695.js"
+  "/vegcart2/precache-manifest.944b1fae429d8352d7b79b4cd8d8f222.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vegcart2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
